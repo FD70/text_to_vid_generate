8230418db17dcf54a025e2fc64edb1ac8e337ebf
FD70000000 <halfl.ez.ser@mail.ru>
2022-06-09 17:04:18 +0300

    Разбитие исходного текста на слова

--- main.py
+++ main.py
@@ -5,26 +5,24 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-path_of_out_video = os.path.join(os.getcwd(), "out_vid")
+INPUT_FILE_PATH = 'input.txt'
+OUTPUT_FILE_PATH = os.path.join(os.getcwd(), "out_vid")
+
+# OUTPUT_FRAME_SHAPE X, Y
+# FRAME = (960, 540)
+FRAME = (1920, 1080)
 FPS_OUT = 60.0
 
-INPUT_FILE_PATH = 'input.txt'
-SAMPLE_TEXT = "ДЕФОРМАЦИЯ"
-FRAME = (960, 540)
-# FRAME = (1920, 1080)
-
+COLOR_WHITE = '#ffffff'
 FONT_SIZE = 48
 
-COLOR_WHITE = '#ffffff'
 
-
-def take_v_writer(filename):
-    # cv2.VideoWriter(filename, fourcc, fps, frameSize)
-    return cv2.VideoWriter(os.path.join(path_of_out_video, f"{filename}.avi"), fourcc, FPS_OUT, (FRAME[0], FRAME[1]))
+def get_video_writer(output_filename: str):
+    return cv2.VideoWriter(os.path.join(OUTPUT_FILE_PATH, f"{output_filename}.avi"), fourcc, FPS_OUT, (FRAME[0], FRAME[1]))
 
 
 def text_to_vid_generate(input_str):
-    image = Image.new('RGB', FRAME, color=(COLOR_WHITE))
+    image = Image.new('RGB', FRAME, color=COLOR_WHITE)
     draw = ImageDraw.Draw(image)
     font_enc = ImageFont.truetype("ArialBD.ttf", size=FONT_SIZE, encoding='UTF-8')
     w, h = draw.textsize(input_str, font_enc)
@@ -36,7 +34,7 @@ def text_to_vid_generate(input_str):
             fill=(0, 0, 0, 255) # цвет
             )
 
-    vout = take_v_writer(input_str)
+    vout = get_video_writer(input_str)
     recframe = numpy.array(image)
     # FIXME: поменять "волшебную" зависимость от FPS_OUT, заменить на необхобимое время отрисовки
     for i in range(int(FPS_OUT) * 5):
@@ -44,54 +42,43 @@ def text_to_vid_generate(input_str):
     vout.release()
 
 
-# FIXME: ПОКА НЕ ГОТОВО, ПОТОМ УБРАТЬ ФИКСМи
 def book_to_vid_generate(txt_file_path):
     OUTPUT_FILE_NAME = "book_to_vid_generate"
-    image_empty = Image.new('RGB', FRAME, color=(COLOR_WHITE))
+    image_empty = Image.new('RGB', FRAME, color=COLOR_WHITE)
     # draw = ImageDraw.Draw(image)
     font_enc = ImageFont.truetype("ArialBD.ttf", size=FONT_SIZE, encoding='UTF-8')
 
-    vout = take_v_writer(OUTPUT_FILE_NAME)
+    vout = get_video_writer(OUTPUT_FILE_NAME)
 
     with open(txt_file_path, 'r', encoding="utf-8") as _file:
         _file_lines = _file.read().splitlines()
 
-    # FIXME: REPLACE FILE LINES WITH - WORDS!
     # FIXME: ADD BUFFER VARIABLE FOR "-"-TYPE SEPARATE SYMBOLS
-    # FIXME: Вероятно, нужно будет убрать изменения отступа в зависимости от высоты текста, чтобы не скакал
     for _one_line in _file_lines:
-        image = copy.copy(image_empty)
-        draw = ImageDraw.Draw(image)
+        for _one_word in _one_line.split():
+            image = copy.copy(image_empty)
+            draw = ImageDraw.Draw(image)
 
-        w, h = draw.textsize(_one_line, font_enc)
+            w, h = draw.textsize(_one_word, font_enc)
 
-        draw.text(
-            ((FRAME[0] - w) // 2, (FRAME[1] - h) // 2),
-            _one_line,
-            font=font_enc,
-            fill=(0, 0, 0, 255)  # TEXT COLOR
-        )
+            draw.text(
+                ((FRAME[0] - w) // 2, (FRAME[1]) // 2),
+                _one_word,
+                font=font_enc,
+                fill=(0, 0, 0, 255)  # TEXT COLOR
+            )
 
-        recframe = numpy.array(image)
+            recframe = numpy.array(image)
 
-        # APPEND 10 FRAMES
-        for i in range(6):
-            vout.write(recframe)
+            # APPEND x FRAMES
+            for i in range(6):
+                vout.write(recframe)
 
     vout.release()
 
 
 if __name__ == "__main__":
-    # with open('input.txt', 'r', encoding="utf-8") as file:
-    #     input_line = file.read().splitlines()
-    #
-    # increment = 0
-    # for one_line in input_line:
-    #     text_to_vid_generate(one_line)
-    #
-    #     increment += 1
-    #     print(increment)
     book_to_vid_generate(INPUT_FILE_PATH)
 
 # FIXME: Сделать якори на затраченное на рендер время
-# FIXME: Реализовать функццию, которая добавляет паузу "-" на заданное в ф-ии время.
+# FIXME: Реализовать функццию, которая добавляет "рисует" паузу "-" на заданное в ф-ии время.
