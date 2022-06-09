f640f65e54a6ab9cdcade94c99f7a652c8d3f0bb
FD70000000 <halfl.ez.ser@mail.ru>
2022-06-09 18:47:42 +0300

    количество оставшихся строк, время полной отработки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy
 import copy
+import time
 import os
 from PIL import Image, ImageDraw, ImageFont
 
@@ -9,13 +10,13 @@ INPUT_FILE_PATH = 'input.txt'
 OUTPUT_FILE_PATH = os.path.join(os.getcwd(), "out_vid")
 
 # OUTPUT_FRAME_SHAPE X, Y
-FRAME = (960, 540)
-# FRAME = (1920, 1080)
+# FRAME = (960, 540)
+FRAME = (1920, 1080)
 FPS_OUT = 60.0
 
 COLOR_WHITE = '#ffffff'
 FONT_SIZE = 48
-# FIXME: вынести text_color
+FONT_COLOR = (13, 42, 13) # BGR
 
 FONT_ENC = ImageFont.truetype("ArialBD.ttf", size=FONT_SIZE, encoding='UTF-8')
 IMAGE_EMPTY = Image.new('RGB', FRAME, color=COLOR_WHITE)
@@ -34,7 +35,7 @@ def pause_generate(video_writer, frames_count: int = 1, symb: str = "-"):
         ((FRAME[0] - w) // 2, (FRAME[1] - h) // 2),
         symb,
         font=FONT_ENC,
-        fill=(0, 0, 0, 255)  # цвет
+        fill=FONT_COLOR
     )
 
     recframe = numpy.array(image)
@@ -43,7 +44,7 @@ def pause_generate(video_writer, frames_count: int = 1, symb: str = "-"):
         video_writer.write(recframe)
 
 
-def text_to_vid_generate(input_str):
+def string_to_vid_generate(input_str):
     image = copy.copy(IMAGE_EMPTY)
     draw = ImageDraw.Draw(image)
 
@@ -53,12 +54,13 @@ def text_to_vid_generate(input_str):
             ((FRAME[0] - w) // 2, (FRAME[1] - h) // 2),
             input_str,
             font=FONT_ENC,
-            fill=(0, 0, 0, 255) # цвет
+            fill=FONT_COLOR
             )
 
     vout = get_video_writer(input_str)
     recframe = numpy.array(image)
     # FIXME: поменять "волшебную" зависимость от FPS_OUT, заменить на необхобимое время отрисовки
+    # FIXME: добивать default- значение в шапку функции
     for i in range(int(FPS_OUT) * 5):
         vout.write(recframe)
     vout.release()
@@ -69,13 +71,18 @@ def book_to_vid_generate(txt_file_path):
 
     vout = get_video_writer(OUTPUT_FILE_NAME)
 
-    pause_generate(vout, 120)
+    pause_generate(vout, 60)
 
     with open(txt_file_path, 'r', encoding="utf-8") as _file:
         _file_lines = _file.read().splitlines()
+        _l_counter = 0
+        lines_count = len(_file_lines)
 
     # FIXME: ADD BUFFER VARIABLE FOR "-"-type SEPARATE SYMBOLS
     for _one_line in _file_lines:
+        print(f"{lines_count - _l_counter} lines")
+        _l_counter += 1
+
         for _one_word in _one_line.split():
             image = copy.copy(IMAGE_EMPTY)
             draw = ImageDraw.Draw(image)
@@ -86,7 +93,7 @@ def book_to_vid_generate(txt_file_path):
                 ((FRAME[0] - w) // 2, (FRAME[1]) // 2),
                 _one_word,
                 font=FONT_ENC,
-                fill=(0, 0, 0, 255)  # TEXT COLOR
+                fill=FONT_COLOR  # TEXT COLOR
             )
 
             recframe = numpy.array(image)
@@ -99,8 +106,11 @@ def book_to_vid_generate(txt_file_path):
 
 
 if __name__ == "__main__":
+
+    start_time = time.time()
+
     book_to_vid_generate(INPUT_FILE_PATH)
 
-# FIXME: Сделать якори на затраченное на рендер время
-# FIXME: Реализовать функццию, которая добавляет "рисует" паузу "-" на заданное в ф-ии время
+    print(f"{time.time() - start_time:.2f} - sec's")
+
 # FIXME: Проверка наличия "выходной папки", перед запуском генерации, создавать папку, при отсутствии
